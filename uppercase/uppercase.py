bb5f35274d7a197e688ab3b39aecf97f6518f9fc
Anne Schumacher <anne.schumacher@gu.se>
2021-04-07 15:17:00 +0200

    add example usage of config variable

--- uppercase/uppercase.py
+++ uppercase/uppercase.py
@@ -5,6 +5,8 @@ from sparv import Annotation, Output, annotator
 
 @annotator("Convert every word to uppercase")
 def uppercase(word: Annotation = Annotation("<token:word>"),
-              out: Output = Output("<token>:uppercase.upper")):
+              out: Output = Output("<token>:uppercase.upper"),
+              # some_config_variable: str = Config("uppercase.some_setting")
+              ):
     """Convert to uppercase."""
     out.write([val.upper() for val in word.read()])
