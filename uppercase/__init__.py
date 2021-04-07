6f12c84eaf8009cd90177305ec90bb0941e48fed
Anne Schumacher <anne.schumacher@gu.se>
2021-04-07 14:31:00 +0200

    add plugin template files

---
+++ uppercase/__init__.py
@@ -0,0 +1,11 @@
+"""Example for a Sparv annotator that converts tokens to uppercase."""
+
+# from sparv import Config
+
+from . import uppercase
+
+# __config__ = [
+#     Config("uppercase.some_setting", "some_default_value", description="Description for this setting")
+# ]
+
+__description__ = "Example for a Sparv annotator that converts tokens to uppercase."
