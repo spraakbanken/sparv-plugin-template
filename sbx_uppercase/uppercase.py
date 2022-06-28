c686f0f54d91d0a2bb8eaa4ffa4fa6351d47bb65
Anne Schumacher <anne.schumacher@gu.se>
2022-06-28 13:35:50 +0200

    introduce module prefix

--- sbx_uppercase/uppercase.py
+++ sbx_uppercase/uppercase.py
@@ -5,8 +5,8 @@ from sparv.api import Annotation, Output, annotator
 
 @annotator("Convert every word to uppercase")
 def uppercase(word: Annotation = Annotation("<token:word>"),
-              out: Output = Output("<token>:uppercase.upper"),
-              # some_config_variable: str = Config("uppercase.some_setting")
+              out: Output = Output("<token>:sbx_uppercase.upper"),
+              # some_config_variable: str = Config("sbx_uppercase.some_setting")
               ):
     """Convert to uppercase."""
     out.write([val.upper() for val in word.read()])
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 import setuptools
 
 setuptools.setup(
-    name="sparv-uppercase",
+    name="sparv-sbx-uppercase",
     version="0.1",
     description="Uppercase converter (example plug-in for Sparv)",
     license="MIT",
     author="",
     author_email="",
-    packages=["uppercase"],
+    packages=["sbx_uppercase"],
     python_requires=">=3.6",
-    install_requires=["sparv-pipeline>=4,<5"],
-    entry_points={"sparv.plugin": ["uppercase = uppercase"]}
+    install_requires=["sparv-pipeline>=5.0.dev0,<6"],
+    entry_points={"sparv.plugin": ["sbx_uppercase = sbx_uppercase"]}
 )
